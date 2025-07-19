194de2589e34961709a2fe473984a9882c7ea4f6
Neha Mehar <nehamehar31@gmail.com>
2025-07-19 21:27:53 +0530

    Add dist for GitHub Pages

---
+++ Grocery.1c01c42c.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire164e;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var l=e("ayMG0"),t=()=>(0,l.jsx)("h3",{children:"Hello here are the Grocery Stores for you!"})});
+//# sourceMappingURL=Grocery.1c01c42c.js.map
